d16e8f51493b3a53985e270395fdab1b79f0ee37
PATRICK :D <119462581+Patrickminipekka@users.noreply.github.com>
2023-07-05 15:49:37 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function preload(){
+
+}
+
+function setup(){
+canvas = createCanvas(300,300);
+canvas.center();
+video=createCapture(VIDEO);
+video.size(300, 300);
+video.hide();
+poseNet=ml5.poseNet(video, modelLoaded);
+poseNet.on("pose",gotPoses);
+}
+
+function draw(){
+image(video,0, 0,300,300);
+}
+
+function take_snapshot(){
+    save("miimagenconfiltro.png");
+}
+
+function modelLoaded(){
+    console.log("poseNet esta inicializado")
+}
+
+function gotPoses(results){
+   
+    if(results.length>0){
+        console.log(results);
+        console.log("nariz_X="+results[0].pose.nose.x);
+        console.log("nariz_Y="+results[0].pose.nose.y);
+    }
+  
+}
